28e9e0995c64709718f173c0076e2fdf4c4b76ad
bharathkc <130267963+testgithubhi@users.noreply.github.com>
2024-02-09 20:09:03 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,24 @@
+// Java program to Swap
+// Two variables
+
+// Driver Class
+public class hello {
+    // main function
+    public static void main(String[] args) {
+        int x = 100, y = 200;
+
+        System.out.println("Before Swap");
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+
+        // Swapping using three
+        // Variables
+        int temp = x;
+        x = y;
+        y = temp;
+
+        System.out.println("After swap");
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+    }
+}
